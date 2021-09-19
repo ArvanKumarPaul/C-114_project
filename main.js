be9bbbad97f4fd924c9b38335a8b6e54fffec19b
ArvanKumarPaul <71247101+ArvanKumarPaul@users.noreply.github.com>
2021-09-19 21:46:27 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+function preload() {};
+
+function setup() {
+
+  canvas = createCanvas(400, 300);
+  canvas.center();
+
+}
+
+function draw() {}
+
+function take_snapshot() {
+
+  save("moustache_img.png");
+
+}
